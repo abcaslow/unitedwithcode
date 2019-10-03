d1db23e2492faa258563515c7957e8c55ecc4145
Bruce Caslow <abcaslow@gmail.com>
2019-10-03 17:38:50 -0400

    Adding a python script the extracts ITPIE collected interface information and formats it in JSON format

---
+++ collection-engine-extract-interface-info.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#
+# Script to extract a block of data from an IT-PIE collection file and
+# output it in JSON format.
+#
+
+import glob
+import re
+import json
+import os
+
+# block_command is a regexp to match beginning and end of block data
+
+block_command = 'cmdShowIpInterfaceB'
+
+# in_block is a flag to indicate if we're processing a block of data
+
+in_block = False
+
+output = []
+
+for fn in glob.glob("genius_test/*"):
+    fh = open(fn)
+
+    # Parse filename, expected to be in IT-PIE format
+
+    fn_fields = os.path.basename(fn).split('_')
+    ipaddr = fn_fields[1]
+    timestamp = fn_fields[2]
+    [date, time] = timestamp.split('-')
+    
+    for line in fh:
+        if re.search(block_command, line):
+            in_block = not in_block
+        if in_block and re.search('YES', line):
+            output.append([ipaddr, date, time, line.rstrip()])
+
+print json.dumps(output, indent=4)
